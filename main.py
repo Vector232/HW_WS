13b38f4d946bf74ce621f8207804658022717cf1
Vlad Troyan <030705vt@mail.ru>
2023-08-01 15:29:02 +0300

    finish commit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import requests
-from bs4 import BeautifulSoup
 import json
 from pprint import pprint
 
@@ -23,31 +22,43 @@ def get_area_id(name):
                 return area['id']
               
     return '0'
-
-def soupprocess(raw_data):
+# готовим нужные данные
+def get_info(raw_data, currency_code = None):
+    data = [{'n':i,
+            'name':note['name'],
+            'alternate_url':note['alternate_url'],
+            'salary':note['salary'],
+            'employer':note['employer']['name'],
+            'area':note['area']['name']} for i, note in enumerate(raw_data['items']) 
+                                         if currency_code == None or currency_code == note['salary']['currency']]
 
     return data
-
-
+# получаем список всех вакансий с указанными в задании ограничениями
 def get_vacancies():
+    # доступ к списку вакансий предоставляется и без авторизации пользователя\приложения
     headers = {'HH-User-Agent': 'api-test-agent'}
     url = 'https://api.hh.ru/vacancies'
-
+    # получаем актуальный АЙДИ городов
     area_1 = get_area_id('Москва')
     area_2 = get_area_id('Санкт-Петербург')
-
-    params = {'area':area_1, 'area':area_2, 'salary': None, 'currency':'USD', 'text':'python', 'search_field': 'name'}
+    # пользуемся языком запросов, описанным в документации
+    text = '(NAME:python) AND (DESCRIPTION: django OR flask)'
+    
+    params = {'area': [area_1, area_2],
+              'text':text,
+              'per_page': 100,
+              'only_with_salary': True,
+              'order_by':'publication_time'}
 
     raw_data = requests.get(url=url, headers=headers, params=params).json()
 
-    data = soupprocess(raw_data)
+    return raw_data
 
-    return data
-
-
+# получаем "сырой" ответ
 raw_data = get_vacancies()
-
-data = raw_data
+# отрезаем ненужную информацию, отсеиваем неподходящие вакансии (которые не смогли отсеять через API) 
+# ПЫСЫ: не нашел способа через get запрос получить только вакансии с ЗП в $
+data = get_info(raw_data, 'USD')
 
 with open(f'data.json', 'w', encoding='UTF-8') as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
